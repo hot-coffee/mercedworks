fedd3563a3aefa8081faa358c53e073650b9190a
greenac <andre1226.dev@gmail.com>
2015-12-12 13:32:51 -0800

    set up to fetch and send instagram token

--- routes/request-handler.js
+++ routes/request-handler.js
@@ -3,6 +3,7 @@
 var jsonResponse = require('./../models/json-response');
 var JsonSender = require('./../models/json-sender');
 var serverOptions = require('./../models/server-options');
+var logger = require('gruew-logger');
 
 function RequestHandler() {
     this.index = function (req, res, next) {
@@ -14,29 +15,38 @@ function RequestHandler() {
     };
 
     this.saveToken = function (req, res, next) {
-        console.log('got the save-token request');
         var payload = req.body;
         var response = null;
         var error = null;
         if (payload.instagramKey) {
-            console.log('the key is:', payload.instagramKey);
+            logger(['the key is:', payload.instagramKey], __filename, false, false);
             response = 'got the key';
         } else {
-            console.log('there is no instagram key in the payload');
+            logger(['no insta key in payload'], __filename, true, false);
             error = 'could not get the key';
         }
 
         jsonResponse(res, error, response);
-
+        console.log('sending:', payload);
         if (!error) {
-            var jsonSender = new JsonSender(serverOptions.instagram);
+            var jsonSender = new JsonSender(serverOptions.instagram, payload);
             jsonSender.send(function (error, data) {
                 if (error) {
-                    console.log('Error failed to send token to server with error', error);
+                    logger(
+                        ['Error failed to send token to server with error', error],
+                        __filename,
+                        true,
+                        false
+                    );
                     return;
                 }
 
-                console.log('sent token to server and got response:', data);
+                logger(
+                    ['sent payload:', payload, 'received response:', data],
+                    __filename,
+                    false,
+                    false
+                );
             });
         }
     };
