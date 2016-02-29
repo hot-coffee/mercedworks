8b6623d956f67b17c245fc70b6d26b3de0ab0712
steven <sduval215@gmail.com>
2016-02-28 18:29:57 -0800

    page states

--- public/javascripts/controllers/profile-controller.js
+++ public/javascripts/controllers/profile-controller.js
@@ -13,6 +13,32 @@ angular.module('MercedWorks').controller(
                     }
 
                     $scope.profile = profileFactory.profileForId($routeParams.profileId);
+
+                    var currentPage = 0;
+
+                    $scope.updatePage = function(){
+                        var interviewObject = $scope.profile.interview[currentPage];
+                        var pic = interviewObject.pic;
+                        var bio = interviewObject.bio;
+
+                        
+                    }
+
+                    $scope.t0clicked= function(){
+                        currentPage = 0; 
+                        updatePage();
+                    }
+
+                    $scope.t1clicked = function(){
+                        currentPage = 1;
+                        updatePage();
+                    }
+
+                    $scope.t2clicked = function(){
+                        currentPage = 2;
+                        updatePage();
+                    }
+
                 });
             };
 
