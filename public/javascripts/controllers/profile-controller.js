3edaba20c932dc363e13133fbc307c733bf106dc
greenac <andre1226.dev@gmail.com>
2016-02-29 08:05:27 -0800

    cleaned up logic in profileController.js and sorted profiles in reverse order

--- public/javascripts/controllers/profile-controller.js
+++ public/javascripts/controllers/profile-controller.js
@@ -8,42 +8,15 @@ angular.module('MercedWorks').controller(
 
 
             var currentPageIndex = 0;
+            $scope.tabPressed = function(value) {
+                if (value === currentPageIndex) {
+                    return;
+                }
 
-            var updateCurrentMedia = function() {
+                currentPageIndex = value;
                 $scope.text = $scope.profile.interviews[currentPageIndex];
                 $scope.picUrl = $scope.profile.pics[currentPageIndex];
             };
-
-            $scope.introPressed = function() {
-                console.log('intro button pressed with page index:', currentPageIndex);
-                if (currentPageIndex === 0) {
-                    return;
-                }
-
-                currentPageIndex = 0;
-                updateCurrentMedia();
-            };
-
-            $scope.workPressed = function() {
-                console.log('work button pressed with page index:', currentPageIndex);
-                if (currentPageIndex === 1 || $scope.profile.interviews.length < 2) {
-                    return;
-                }
-
-                currentPageIndex = 1;
-                updateCurrentMedia();
-            };
-
-            $scope.plansPressed = function() {
-                console.log('plans button pressed with page index:', currentPageIndex);
-                if (currentPageIndex === 2 || $scope.profile.interviews.length < 3) {
-                    return;
-                }
-
-                currentPageIndex = 2;
-                updateCurrentMedia();
-            };
-
 
             var init = function() {
                 profileFactory.getProfiles(function(error) {
